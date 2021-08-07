4ea4463735acbd21130ab10cfd437d97fd32ed91
CronyAkatsuki <crolinuxgamer@gamil.com>
2021-08-07 12:00:45 +0200

    updated ryzenadj heavy gaming pro profile scripts

--- ryzenadj-heavy gaming-pro.cmd
+++ ryzenadj-heavy gaming-pro.cmd
@@ -1,2 +1,2 @@
-ryzenadj --slow-time=60 --vrmmax-current=70000 --max-gfxclk=900 --max-fclk-frequency=1200 --tctl-temp=89 --stapm-limit=30000 --stapm-time=1000 --fast-limit=35000 --slow-limit=33000
+ryzenadj --slow-time=60 --vrmmax-current=70000 --max-gfxclk=900 --max-fclk-frequency=1200 --tctl-temp=90 --stapm-limit=20000 --stapm-time=1000 --fast-limit=25000 --slow-limit=21000
 pause
