a3054017a4934938874da5a6cff9728e9cf74c97
CronyAkatsuki <crolinuxgamer@gamil.com>
2021-08-26 09:25:04 +0200

    Added genshin impact profile.

--- ryzenadj/genshin-impact.cmd
+++ ryzenadj/genshin-impact.cmd
@@ -1,2 +1,2 @@
 @echo off
-ryzenadj --slow-time=30 --vrmmax-current=45000 --tctl-temp=70 --stapm-limit=14000 --stapm-time=300 --fast-limit=17000 --slow-limit=15000 --max-performance
+ryzenadj --slow-time=60 --vrmmax-current=45000 --tctl-temp=75 --stapm-limit=12000 --stapm-time=1000 --fast-limit=15000 --slow-limit=13000 --max-performance
