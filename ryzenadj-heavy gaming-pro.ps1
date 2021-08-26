a167f8bcdcacd914468ab5593e65235e82a73e41
CronyAkatsuki <crolinuxgamer@gamil.com>
2021-08-26 09:24:34 +0200

    Updated basic profiles.

--- ryzenadj-heavy gaming-pro.ps1
+++ ryzenadj-heavy gaming-pro.ps1
@@ -1,1 +1,1 @@
-ryzenadj --slow-time=60 --vrmmax-current=70000 --tctl-temp=90 --stapm-limit=20000 --stapm-time=1000 --fast-limit=25000 --slow-limit=21000 --max-performance
+sudo ryzenadj --slow-time=60 --vrmmax-current=70000 --tctl-temp=90 --stapm-limit=20000 --stapm-time=1000 --fast-limit=25000 --slow-limit=21000 --max-performance
--- ryzenadj-light-gaming.cmd
+++ ryzenadj-light-gaming.cmd
@@ -1,2 +1,2 @@
-ryzenadj --slow-time=30 --vrmmax-current=35000 --tctl-temp=65 --stapm-limit=10000 --stapm-time=300 --fast-limit=12000 --slow-limit=11000 --power-saving
+ryzenadj --slow-time=30 --vrmmax-current=35000 --tctl-temp=65 --stapm-limit=10000 --stapm-time=300 --fast-limit=12000 --slow-limit=11000 --max-performance
 pause
