2cf80a2b8434efef89416f0e3e180b8fc1fdb31d
CronyAkatsuki <crolinuxgamer@gamil.com>
2021-11-09 12:46:24 +0100

    Update

---
+++ install-all-my-scoop-packages.bat
@@ -0,0 +1,3 @@
+scoop install 7zip aria2 AtomicParsley bat ffmpeg fzf git hugo hugo-extended islc less lsd mpv-git neovim ninja nodejs-lts pcsx2 pshazz ripgrep ryzenadj sudo
+
+# in administrator run scoop install SourceCodePro-NF
