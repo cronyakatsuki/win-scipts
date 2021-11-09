61dbaffb9249d2be5517b3d86d1ab0cd51251bb0
CronyAkatsuki <crolinuxgamer@gamil.com>
2021-11-09 12:26:31 +0100

    Update

--- ryzenadj/pcsx2.cmd
+++ ryzenadj/pcsx2.cmd
@@ -1,2 +1,2 @@
 @echo off
-ryzenadj --slow-time=60 --vrmmax-current=70000 --tctl-temp=85 --stapm-limit=16000 --stapm-time=1000 --fast-limit=25000 --slow-limit=20000 --max-performance
+ryzenadj --slow-time=60 --vrmmax-current=60000 --tctl-temp=80 --stapm-limit=17000 --stapm-time=1000 --fast-limit=20000 --slow-limit=28000 --max-performance
